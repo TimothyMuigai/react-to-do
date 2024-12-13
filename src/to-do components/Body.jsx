434362d60c97c184791c74c057564bf86204ff87
TimothyMuigai <timothy.muigai@strathmore.edu>
2024-12-13 09:25:14 +0300

    added search and filter

--- src/to-do components/Body.jsx
+++ src/to-do components/Body.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 
-function Body({userData, handleInput, markDone, deleteTask, displayData, userInput}) {
+function Body({setQuery,query, 
+              handleFilter,
+              markDone,deleteTask, 
+              handleInput,userInput,
+              showData,displayData}) 
+              {
   return (
-    <div className="body">
-      <div className='container'>
-      <h2>To-do List</h2>
+      <div className="body">
+
+        <div className='container'>
+          <h2>To-do List</h2>
+
           <div className='list-container'>
               <input type="text"
                   value={userInput}
@@ -13,38 +20,44 @@ function Body({userData, handleInput, markDone, deleteTask, displayData, userInp
               />
               <button onClick={displayData}>Add to list</button>
           </div>
-          <div className='search-container' style={{display:'none'}}>
-              <input type="text" />
-              <button>Search</button>
+
+          <div className='search-container' >
+              <input type="text" 
+                placeholder='Search for a Task'
+                value={query}
+                onChange={e => setQuery(e.target.value)}
+              />
           </div>
+
           <div className='filter-container'>
-              <select name="Filter" id="selectBox" defaultValue="default">
+              <select name="Filter" id="selectBox" defaultValue="no-filter" onChange={handleFilter}>
                   <option value="default" disabled>Filter Task</option>
                   <option value="no-filter">None</option>
                   <option value="complete">Completed Tasks</option>
                   <option value="inComplete">Yet to be Completed</option>
               </select>
           </div>
+
           <div className="display-container">
-          <ul>
-            {userData.map((data, index) => (
-              <li key={index}>
-                <input
-                  type="checkbox"
-                  checked={data.isChecked}
-                  onChange={() => markDone(index)}
-                />
-                <span
-                  style={{ textDecoration: data.isChecked ? 'line-through' : 'none' }}>
-                  {data.newInput}
-                </span>
-                <button onClick={() => deleteTask(index)}>Delete</button>
-              </li>
-            ))}
-          </ul>
+            <ul>
+              {showData.map((data, index) => (
+                <li key={index}>
+                  <input
+                    type="checkbox"
+                    checked={data.isChecked}
+                    onChange={() => markDone(index)}
+                  />
+                  <span
+                    style={{ textDecoration: data.isChecked ? 'line-through' : 'none' }}>
+                    {data.newInput}
+                  </span>
+                  <button onClick={() => deleteTask(index)}>Delete</button>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
   )
 }
 
--- src/to-do components/Functions.jsx
+++ src/to-do components/Functions.jsx
@@ -1,27 +1,50 @@
-import React, { useState } from 'react'
+import React, { useState,useMemo } from 'react'
 import Body from './body';
 
 function Functions() {
   const [userData, setuserData] = useState([]);
   const [inputData, setinputData]=useState('');
+  const [filter, setFilter] =useState('no-filter')
 
-  const handleCheckboxChange = (index) => {
-    setuserData(userData.map((task,i)=>
-      i ==index?{...task, isChecked: !task.isChecked}:task
-    )); 
-  };
-
-  const handleInput = (e)=>{
-    setinputData(e.target.value);
-  }
   const showInput=()=>{
     const newData = {newInput:inputData, isChecked: false};
     setuserData(u=>[...u,newData]);
     setinputData('');
   };
+
+  const handleInput = (e)=>{
+    setinputData(e.target.value);
+  }
+  
+  const handleFilter = (e)=>{
+    setFilter(e.target.value);
+  }
+  const handleCheckboxChange = (index) => {
+    setuserData((prev) =>
+      prev.map((task,i)=>
+      i ==index?{...task, isChecked: !task.isChecked}:task
+    )); 
+  };
+
+  const filteredItems = userData.filter((task) => {
+    if (filter === "no-filter") return true;
+        if (filter === "complete") return task.isChecked;
+        if (filter === "inComplete") return !task.isChecked;
+        return true;
+  });
+
   const delTask=(id)=>{
     setuserData(userData.filter((_,index) => index!==id ))
   }
+
+  const [query, setQuery]= useState('');
+  const displayedItems = useMemo(() => {
+    const itemsToSearch = filter === "no-filter" ? userData : filteredItems;
+    return itemsToSearch.filter((task) =>
+      task.newInput.toLowerCase().includes(query.toLowerCase())
+    );
+  }, [query, filteredItems, userData, filter]);
+  
 
   return (
     <Body
@@ -30,7 +53,10 @@ function Functions() {
       deleteTask = {delTask}
       markDone = {handleCheckboxChange}
       handleInput ={handleInput}
-      userData ={userData}
+      handleFilter ={handleFilter}
+      setQuery ={setQuery}
+      value = {query}
+      showData = {displayedItems}      
     />    
   )
 }
